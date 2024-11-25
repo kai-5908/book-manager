fafaab8a8642dd42e2aad939cbf1bbfc4a0bacdc
kai-5908 <dolce.cantabile.maestoso@gmail.com>
2024-11-25 22:17:09 +0900

    format

--- adapter/src/redis/mod.rs
+++ adapter/src/redis/mod.rs
@@ -11,26 +11,17 @@ pub struct RedisClient {
 
 impl RedisClient {
     pub fn new(config: &RedisConfig) -> AppResult<Self> {
-        let client =
-            Client::open(format!("redis://{}:{}", config.host, config.port))?;
-        Ok(Self {client})
+        let client = Client::open(format!("redis://{}:{}", config.host, config.port))?;
+        Ok(Self { client })
     }
 
-    pub async fn set_ex<T: RedisKey>(
-        &self,
-        key: &T,
-        value: &T::Value,
-        ttl: u64,
-    ) -> AppResult<()> {
+    pub async fn set_ex<T: RedisKey>(&self, key: &T, value: &T::Value, ttl: u64) -> AppResult<()> {
         let mut conn = self.client.get_multiplexed_async_connection().await?;
         conn.set_ex(key.inner(), value.inner(), ttl).await?;
         Ok(())
     }
 
-    pub async fn get<T: RedisKey>(
-        &self,
-        key: &T,
-    ) -> AppResult<Option<T::Value>> {
+    pub async fn get<T: RedisKey>(&self, key: &T) -> AppResult<Option<T::Value>> {
         let mut conn = self.client.get_multiplexed_async_connection().await?;
         let result: Option<String> = conn.get(key.inner()).await?;
         result.map(T::Value::try_from).transpose()
